13481c3042b6609457c7aea0aa6fb74b294e930a
Ziwei Ba <ziweichristina@gmail.com>
2016-04-25 05:16:24 -0500

    everything except for the chat bar

--- bookexchange/bookexchange.js
+++ bookexchange/bookexchange.js
@@ -1,12 +1,17 @@
 Router.route('/login');
 Router.route('/',{
-  name:'home',
+  name:'homepage',
   template:'home'
 });
+Router.route('/home');
 Router.route('/donation_page');
 Router.route('/redeem_page');
+Router.route('/browse');
+Router.route('/forum');
 Router.route('/profile_page');
 Router.route('/logout');
+Router.route('/bookpage');
+
 
 if (Meteor.isClient) {
 
